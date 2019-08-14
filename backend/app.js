9dcc56d6a23f3544a9f661dfea391f86b2586999
Vincent <icwei9@gmail.com>
2019-08-14 22:37:08 +1000

    Backend setup resolved CORS issue

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
 
-const router = express.Router();
-const API_PORT = 3001;
+const API_PORT = 5000;
 
-router.get('api/friendlist', (res, req) => {
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.get('/friendlist', (req, res) => {
   const friend_list = [
     {
       id: 1,
